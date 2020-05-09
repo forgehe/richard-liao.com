a76ce5e489b3c0bd8d92110acc16e379f34fd6f6
forgehe <liao098@gmail.com>
2020-05-08 18:52:34 -0700

    initial scaffold

---
+++ richard-liao-home/src/Profile/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "normalize.css";
+
+export default function Profile() {
+  return (
+    <div>
+      <p>This is Profile</p>
+    </div>
+  );
+}
--- richard-liao-home/src/template.jsx
+++ richard-liao-home/src/template.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import { Container } from "@material-ui/core";
-import { } from "@material-ui/icons";
 
-const useStyles = makeStyles({})
 
 
 
 export default function Template() {
-  const classes = useStyles();
 
   return ()
 }
