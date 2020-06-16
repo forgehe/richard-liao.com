a1c2426977ea1552fcdf85cc8c86d1084feb2e25
forgehe <liao098@gmail.com>
2020-06-16 15:00:38 -0700

    Inital commit - Profile

--- richard-liao-home/src/Profile/index.jsx
+++ richard-liao-home/src/Profile/index.jsx
@@ -3,8 +3,17 @@ import "normalize.css";
 
 export default function Profile() {
   return (
-    <div>
+    <article>
       <p>This is Profile</p>
-    </div>
+      <h2>About Me</h2>
+      <p>
+        I am a fullstack web developer who studied at Lighthouse Labs in their 3
+        month, 12hr/day Web Development bootcamp. I always had a passion for
+        creating websites and other related media.
+      </p>
+      <h3>Education</h3>
+      <h3>Interests</h3>
+      <h3>Links</h3>
+    </article>
   );
 }
