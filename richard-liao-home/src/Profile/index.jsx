ee0e848946f71485b949a221c3258a5897562ef1
forgehe <liao098@gmail.com>
2020-07-16 21:31:38 -0700

    Add divs

--- richard-liao-home/src/Profile/index.jsx
+++ richard-liao-home/src/Profile/index.jsx
@@ -6,26 +6,39 @@ import "./profile.scss";
 
 export default function Profile() {
   return (
-    <article>
-      <p>This is Profile</p>
-      <h2>About Me</h2>
-      <p>
+    <article className="profile">
+      <p className="profile-description">
         I am a fullstack web developer who studied at Lighthouse Labs in their 3
         month, 12hr/day Web Development bootcamp. I always had a passion for
         creating websites and other related media. I enjoy experimenting with
         new ideas, and I always keep up with the latest UI/UX trends.
       </p>
-      <h3>Education</h3>
-      <p></p>
-      <h3>Skills</h3>
-      <Carosel
-        className="profile-carosel"
-        images={skillsImages}
-        visibleSlides="5"
-        naturalSlideHeight="1"
-        naturalSlideWidth="2"
-      />
-      <h3>Find me elsewhere:</h3>
+      <div className="profile-education">
+        <h3>Education</h3>
+        <p>Lighthouse Labs - April 2020</p>
+      </div>
+      <div className="profile-skills">
+        <h3 className="profile-title">Skills</h3>
+        <Carosel
+          className="profile-carosel"
+          images={skillsImages}
+          visibleSlides="3"
+          naturalSlideHeight="1"
+          naturalSlideWidth="2"
+        />
+        <nav>
+          <h3>Find me elsewhere:</h3>
+          <ul>
+            <li>
+              <a href="https://www.linkedin.com/in/richard-r-liao/">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://github.com/forgehe">GitHub</a>
+            </li>
+            <li>Fiverr (coming soon)</li>
+          </ul>
+        </nav>
+      </div>
     </article>
   );
 }
