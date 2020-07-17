f72b42c4e6edc0024e319a84b4a6dae361ce6429
forgehe <liao098@gmail.com>
2020-07-17 13:13:17 -0700

    add github links

--- richard-liao-home/src/projects/projectsList.js
+++ richard-liao-home/src/projects/projectsList.js
@@ -13,6 +13,7 @@ const projectsList = [
     stack:
       "Frontend using React with Material UI, Backend with Ruby on Rails API connected to an ElepantSQL database.",
     link: "https://meezr.richard-liao.com",
+    github: "https://github.com/pratikbassi/Meezr",
     images: meezrImages,
   },
   {
@@ -20,14 +21,14 @@ const projectsList = [
     description:
       "Made in Ruby on Rails, this is to learn about implementing new features onto an existing project.",
     stack: "Frontend using ERB, Backend with Ruby on Rails.",
-    link: "https://github.com/forgehe/jungle-app",
+    github: "https://github.com/forgehe/jungle-app",
     images: jungleAppImages,
   },
   {
     title: "Scheduler",
     description:
       "Made with React, this is a website to schedule meetings created to learn about React and updating state.",
-    link: "https://github.com/forgehe/scheduler",
+    github: "https://github.com/forgehe/scheduler",
     images: schedulerImages,
   },
   {
@@ -36,21 +37,21 @@ const projectsList = [
       "Made with Express and JQuery, this was a group midterm project to create a online buy/sell marketplace.",
     stack:
       "Frontend using SASS and JQuery. Backend using ExpressJS and PostgreSQL.",
-    link: "https://github.com/pratikbassi/kupit",
+    github: "https://github.com/pratikbassi/kupit",
     images: kupitImages,
   },
   {
     title: "Tweeter",
     description:
       "Made with Express and JQuery, this is a simple twitter clone to learn about AJAX calls.",
-    link: "https://github.com/forgehe/tweeter",
+    github: "https://github.com/forgehe/tweeter",
     images: tweeterImages,
   },
   {
     title: "Tinyapp",
     description:
       "Made with Express and EJS, this is a website to shorten URLs, created to learn about Express and EJS files.",
-    link: "https://github.com/forgehe/tinyapp",
+    github: "https://github.com/forgehe/tinyapp",
     images: tinyappImages,
   },
 ];
