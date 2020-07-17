48625b367d943bc009cb228810fd72bafa0de1c8
forgehe <liao098@gmail.com>
2020-07-17 13:14:29 -0700

    update view for grid

--- richard-liao-home/src/Portfolio/index.jsx
+++ richard-liao-home/src/Portfolio/index.jsx
@@ -4,15 +4,25 @@ import "normalize.css";
 import "./portfolio.scss";
 
 export default function Portfolio(props) {
-  const { images, title, description, link } = props;
+  const { images, title, description, link, github } = props;
   console.log(images);
   return (
     <section className="project">
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <h4>{title}</h4>
-      </a>
-      <p>{description}</p>
-      <Carosel className="portfolio-carosel" images={images} />
+      <div className="project-header">
+        <h4 className="project-header-title">{title}</h4>
+      </div>
+      <p className="project-description">{description}</p>
+      <div className="project-carosel">
+        <Carosel images={images} />
+        <div className="project-carosel-links">
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            View Site
+          </a>
+          <a href={github} target="_blank" rel="noopener noreferrer">
+            View Github
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
