85cb7b7709cc6d4ee0391de899c4d5c44f2521ac
forgehe <liao098@gmail.com>
2020-07-29 13:53:28 -0700

    Add stack, and rearrange project buttons

--- richard-liao-home/src/Portfolio/index.jsx
+++ richard-liao-home/src/Portfolio/index.jsx
@@ -4,24 +4,35 @@ import "normalize.css";
 import "./portfolio.scss";
 
 export default function Portfolio(props) {
-  const { images, title, description, link, github } = props;
-  console.log(images);
+  const { images, title, description, link, github, stack } = props;
+  // console.log(images);
   return (
     <section className="project" id={`project-` + title}>
       <div className="project-header">
         <h4 className="project-header-title">{title}</h4>
+        <h5 className="project-header-stack">{"Stack: " + stack}</h5>
       </div>
-      <p className="project-description">{description}</p>
+      <div className="project-content">
+        <p className="project-description">{description}</p>
+        <a
+          className="project-content-buttons"
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Site
+        </a>
+        <a
+          className="project-content-buttons"
+          href={github}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Github
+        </a>
+      </div>
       <div className="project-carosel">
         <Carosel images={images} />
-        <div className="project-carosel-links">
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            View Site
-          </a>
-          <a href={github} target="_blank" rel="noopener noreferrer">
-            View Github
-          </a>
-        </div>
       </div>
     </section>
   );
