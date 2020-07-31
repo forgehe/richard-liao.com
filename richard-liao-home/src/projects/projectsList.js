72dc2ee1b1d57266cc6e99e4a152b30e916fed27
forgehe <liao098@gmail.com>
2020-07-31 16:15:45 -0700

    update descriptions

--- richard-liao-home/src/projects/projectsList.js
+++ richard-liao-home/src/projects/projectsList.js
@@ -9,7 +9,7 @@ const projectsList = [
   {
     title: "Meezr",
     description:
-      "A group project for our finals to create a meal planner website, pulling data from the spoonacular api. I was mostly in charge of the frontend features in React, while also helping out with getting and transforming the data pulled from the spoonacular API.",
+      "A group final project for Lighthouse Labs to create a meal planner website, pulling data from the spoonacular api. I was mostly in charge of the frontend features in React, while also helping out with getting and transforming the data pulled from the spoonacular API.",
     stack:
       "Frontend using React with Material UI, Backend with Ruby on Rails API connected to an ElepantSQL database.",
     link: "https://meezr.richard-liao.com",
@@ -19,7 +19,7 @@ const projectsList = [
   {
     title: "Jungle",
     description:
-      "Made in Ruby on Rails, this is to learn about implementing new features onto an existing project.",
+      "This project was to get experience with implementing new features onto an existing project. We were tasked with adding a new admin page, fixing visual bugs on the site, and implementing the checkout page using Stripe's API.",
     stack: "Frontend using ERB, Backend with Ruby on Rails.",
     github: "https://github.com/forgehe/jungle-app",
     images: jungleAppImages,
@@ -27,30 +27,33 @@ const projectsList = [
   {
     title: "Scheduler",
     description:
-      "Made with React, this is a website to schedule meetings created to learn about React and updating state.",
+      "A website for scheduling meetings, created to learn about functional React and how updating states works in React.",
+    stack: "Frontend using React. Testing with Cypress and Storybook.",
     github: "https://github.com/forgehe/scheduler",
     images: schedulerImages,
   },
   {
     title: "kupit",
     description:
-      "Made with Express and JQuery, this was a group midterm project to create a online buy/sell marketplace.",
+      "This was a group midterm project to create an online buy/sell marketplace. Main contributions was to the inital html scaffolding, and ",
     stack:
-      "Frontend using SASS and JQuery. Backend using ExpressJS and PostgreSQL.",
+      "Frontend using Bootstrap, SASS and JQuery. Backend with ExpressJS and PostgreSQL.",
     github: "https://github.com/pratikbassi/kupit",
     images: kupitImages,
   },
   {
     title: "Tweeter",
     description:
-      "Made with Express and JQuery, this is a simple twitter clone to learn about AJAX calls.",
+      "A simple twitter clone to learn about AJAX calls and CSS styling.",
+    stack: "Frontend using JQuery and SCSS. Backend with ExpressJS.",
     github: "https://github.com/forgehe/tweeter",
     images: tweeterImages,
   },
   {
     title: "Tinyapp",
     description:
-      "Made with Express and EJS, this is a website to shorten URLs, created to learn about Express and EJS files.",
+      "A website to shorten URLs, created to learn about Express and EJS files. ",
+    stack: "Frontend using JavaScript and HTML. Backend with ExpressJS.",
     github: "https://github.com/forgehe/tinyapp",
     images: tinyappImages,
   },
